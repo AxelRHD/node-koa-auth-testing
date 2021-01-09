62b98f67bd4d09db50aeeb6e8879ff9eab03b066
AxelRHD <axelrhd@gmail.com>
2021-01-09 23:14:57 +0100

    testing template engine

--- router.js
+++ router.js
@@ -1,24 +1,29 @@
-const jwt = require('jsonwebtoken')
-const Router = require('@koa/router')
+const jwt = require("jsonwebtoken")
+const Router = require("@koa/router")
 
-const { jwt_secrets, jwt_cookie_name } = require('./utils')
+const { jwt_secrets, jwt_cookie_name } = require("./utils")
 
 const router = new Router()
 const token = jwt.sign({ sub: 1 }, jwt_secrets[0])
 
-router.get('/public/auth', async ctx => {
-  ctx.body = "You got a token!"
-  ctx.cookies.set(jwt_cookie_name, token)
+router.get("/", async (ctx) => {
+ //ctx.body = "Hello from Koa!"
+ await ctx.render("main")
 })
 
-router.get('/protected', async ctx => {
-  ctx.body = ctx.state.jwtdata
+router.get("/public/auth", async (ctx) => {
+ ctx.body = "You got a token!"
+ ctx.cookies.set(jwt_cookie_name, token)
 })
 
-router.get('/logout', async ctx => {
-  ctx.cookies.set(jwt_cookie_name, '', { maxAge: new Date() })
+router.get("/protected", async (ctx) => {
+ ctx.body = ctx.state.jwtdata
+})
 
-  ctx.body = 'Cookie deleted.'
+router.get("/logout", async (ctx) => {
+ ctx.cookies.set(jwt_cookie_name, "", { maxAge: new Date() })
+
+ ctx.body = "Cookie deleted."
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,33 +1,35 @@
-const Koa = require('koa')
-const KoaJwt = require('koa-jwt')
+const Koa = require("koa")
+const KoaJwt = require("koa-jwt")
+const views = require("koa-views")
 
-const { jwt_secrets, jwt_cookie_name } = require('./utils')
-const router = require('./router')
+const { jwt_secrets, jwt_cookie_name } = require("./utils")
+const router = require("./router")
 
 const app = new Koa()
 
-const port = app.env === 'development' ? 1234 : process.env.port || 8090
+const port = app.env === "development" ? 1234 : process.env.port || 8090
+const render = views(__dirname + "/views", {
+ extension: "marko"
+})
 
 // MIDDLEWARE
-app
-  .use(KoaJwt({
-    cookie: jwt_cookie_name,
-    key: 'jwtdata',
-    secret: jwt_secrets
-  })
-  .unless({
-    path: [
-      /^\/public/
-    ]
-  }))
+app.use(render)
+app.use(
+ KoaJwt({
+  cookie: jwt_cookie_name,
+  key: "jwtdata",
+  secret: jwt_secrets
+ }).unless({
+  path: [/^\/$/, /^\/public/]
+ })
+)
 
 // ROUTES
 
-
 // APPLICATION
 app
-  .use(router.routes())
-  .use(router.allowedMethods())
-  .listen(port, () => {
-    `Server listening on port ${port}`
-  })
+ .use(router.routes())
+ .use(router.allowedMethods())
+ .listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+ })
